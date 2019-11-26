a282b64090044b9f0a05a648b91780ae412110b7
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2019-11-27 01:34:00 +0900

    add collect-bus-locations lambda function

---
+++ poptat-collect-bus-locations/lambda_function.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+import json
+import gzip
+
+import boto3
+
+import requests
+
+def lambda_handler(event, context):
+  now = datetime.datetime.utcnow()
+  year, month, day = now.year, now.month, now.day
+  s3 = boto3.client("s3")
+  buses = json.loads(gzip.decompress(s3.get_object(Bucket="poptat", Key="static/bus-locations.json.gz")["Body"].read()))
+  for bus in buses:
+    url = f'https://bus.kakao.com/bus/{bus}/locations'
+    key = f'bus/locations/{bus}/dt={year}.{month}.{day}/{now.isoformat()}.json.gz'
+    body = gzip.compress(requests.get(url).content)
+    s3.put_object(Bucket="poptat", Key=key, Body=body)
+  return { "statusCode": 200 }
+
